48e3295df319c4708db2251dc381c9026dc68904
Brian Nduhiu <briannduhiu@gmail.com>
2022-11-17 12:26:06 +0300

    form layout update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
  <!-- component -->
+
+
  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-  <div class="w-full max-w-md space-y-8">
+  <div class="w-full max-w-xl space-y-8"> 
     <div>
       <img class="mx-auto h-12 w-auto" src="https://getwaya.com/wp-content/uploads/2022/10/Waya-Alt-1.png" alt="Your Company">
       <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign Up</h2>
@@ -13,62 +15,62 @@
         <!--First Name-->
         <div>
           <label for="first-name" class="sr-only">First name</label>
-          <input id="first-name" name="first-name" type="text" autocomplete="first name" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="First name">
+          <input id="first-name" name="first-name" type="text" autocomplete="first name" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="First name">
         </div>
         <!--Middle Name-->
         <div>
           <label for="middle-name" class="sr-only">Middle name</label>
-          <input id="middle-name" name="middle-name" type="text" autocomplete="middle name" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Middle name">
+          <input id="middle-name" name="middle-name" type="text" autocomplete="middle name" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Middle name">
         </div>
         <!--Last Name-->
         <div>
           <label for="last-name" class="sr-only">Last name</label>
-          <input id="last-name" name="last-name" type="text" autocomplete="last name" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Last name">
+          <input id="last-name" name="last-name" type="text" autocomplete="last name" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Last name">
         </div>
         <!--Email-->
         <div>
           <label for="email-address" class="sr-only">Email address</label>
-          <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address">
+          <input id="email-address" name="email" type="email" autocomplete="email" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address">
         </div>
         <!--Mobile-->
         <div>
           <label for="mobile" class="sr-only">Mobile</label>
-          <input id="mobile" name="mobile" type="tel" autocomplete="mobile" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Mobile number">
+          <input id="mobile" name="mobile" type="tel" autocomplete="mobile" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Mobile number">
         </div>
         <!--SSN-->
         <div>
           <label for="ssn" class="sr-only">Social Security Number</label>
-          <input id="ssn" name="ssn" type="number" autocomplete="social security number" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Social security number">
+          <input id="ssn" name="ssn" type="number" autocomplete="social security number" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Social security number">
         </div>
         <!--Address-->
         <div>
           <label for="adress" class="sr-only">Address</label>
-          <input id="adress" name="adress" type="text" autocomplete="address" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Address">
+          <input id="adress" name="adress" type="text" autocomplete="address" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Address">
         </div>
         <!--city-->
         <div>
           <label for="city" class="sr-only">City</label>
-          <input id="city" name="city" type="text" autocomplete="city" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="City">
+          <input id="city" name="city" type="text" autocomplete="city" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="City">
         </div>
         <!--state-->
         <div>
           <label for="state" class="sr-only">State</label>
-          <input id="state" name="state" type="text" autocomplete="state" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="State">
+          <input id="state" name="state" type="text" autocomplete="state" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="State">
         </div>
         <!--postal-code-->
         <div>
           <label for="postal-code" class="sr-only">Postal code</label>
-          <input id="postal-code" name="postal-code" type="number" autocomplete="postal-code" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Postal code">
+          <input id="postal-code" name="postal-code" type="number" autocomplete="postal-code" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Postal code">
         </div>
         <!--birthday-->
         <div>
           <label for="birthday" class="sr-only">Date of birth</label>
-          <input id="birthday" name="birthday" type="date" autocomplete="birthday" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Date of Birth">
+          <input id="birthday" name="birthday" type="date" autocomplete="birthday" required class="mb-1.5 relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Date of Birth">
         </div>
         <!--Password-->
         <div>
           <label for="password" class="sr-only">Password</label>
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password">
         </div>
       </div>
 
